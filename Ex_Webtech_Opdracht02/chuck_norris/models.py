c7bc5e90e29f2eef6f79aec8defc97a45e33595d
Bramver <Bramvereertbrugghen@outlook.com>
2016-04-12 18:49:47 +0200

    Added REDIS functionality to the project, able to show view on startup. /endfunctionality

--- Ex_Webtech_Opdracht02/chuck_norris/models.py
+++ Ex_Webtech_Opdracht02/chuck_norris/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+class Quote(models.Model):
+	quote_author_fname = models.CharField(max_length=50)
+	quote_author_lname = models.CharField(max_length=50)
+	quote_text = models.CharField(max_length=300)
+
+	def __str__(self):
+		return self.quote_text
--- Ex_Webtech_Opdracht02/chuck_norris/views.py
+++ Ex_Webtech_Opdracht02/chuck_norris/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-
+from django.views import generic
 from .forms import NameForm
+from .models import Quote
 
-# Create your views here.
+import redis
+
+# Create your models here.
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+class IndexView(generic.ListView):
+    template_name = 'chuck_norris/index.html'
+
+    def get_queryset(self):
+    	return "Hello I sok"
+
+
 def get_first_name(request):
 	if request.method == 'POST':
 		form = NameForm(request.POST)
